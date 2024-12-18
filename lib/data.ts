8967843f80c35e133ca7de18c8e9e1b3e12db181
Matt Mandersheid <mmandersheid@gmail.com>
2024-12-18 18:17:36 -0500

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -77,7 +77,7 @@ export const experiencesData = [
 ] as const;
 
 export const projectsData = [
-  {/*
+  {
     title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
@@ -98,7 +98,7 @@ export const projectsData = [
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
     imageUrl: wordanalyticsImg,
   },
-*/}
+
 ] as const;
 
 export const skillsData = [
